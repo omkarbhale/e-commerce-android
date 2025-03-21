9ab2773a3ee03b34724d0c3897fcce1184dc6e30
John Doe <johndoe@example.com>
2025-03-21 22:32:01 +0530

    formatting yes finally

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,19 @@
-import { Slot, Stack, Tabs, useRouter } from 'expo-router';
-import { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Slot, Stack, Tabs, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
 
 const isLoggedIn = false;
 
 export default function Layout() {
-  const router = useRouter()
+	const router = useRouter();
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      router.replace("/login/login1");
-    } else {
-      router.replace("/+not-found");
-    }
-  })
+	useEffect(() => {
+		if (!isLoggedIn) {
+			router.replace("/login/login1");
+		} else {
+			router.replace("/+not-found");
+		}
+	});
 
-  return <Slot />
+	return <Slot />;
 }
